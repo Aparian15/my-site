4292f2bb8375d0600c792d623140ecede9b81fb8
ArianAP <aparian15@icloud.com>
2022-01-27 13:23:02 +0330

    first page created

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".mainContainer");
+function activeAdd(){
+    container.classList.add("active");
+}
+function activeRemove() {
+    container.classList.remove("active");
+}
